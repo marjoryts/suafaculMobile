09d62f417bdfb0a2015988f4292dc5a2778fde3d
marxrjory <206086986+marjoryts@users.noreply.github.com>
2026-05-10 21:25:20 -0300

    Update App.jsx
    troca do import

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/stack'; 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { useAppFonts } from './use-fonts';
